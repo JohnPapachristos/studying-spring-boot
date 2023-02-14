0114a5591613c45151292aa84123e8968a91edce
John Papachristos <88382457+JohnPapachristos@users.noreply.github.com>
2023-02-14 17:48:53 +0200

    Add files via upload

---
+++ src/main/java/com/sprintBoot/project/studyingspringboot/StudyingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sprintBoot.project.studyingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyingSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprintBoot/project/studyingspringboot/StudyingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintBoot.project.studyingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
